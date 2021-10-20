45fffcb0022d00fb323e30123afe6f99e4e72745
malwinakochman <92380332+malwinakochman@users.noreply.github.com>
2021-10-20 15:34:49 +0200

    Add files via upload

---
+++ beforelesson3_ex4.1.py
@@ -0,0 +1,15 @@
+successful = True
+for number in range(3):
+    print("Attempt")
+    if successful:
+        print("Successful")
+        break
+else:
+    print("Attempted 3 times and failed.")
+    
+
+for x in "Python":
+    print(x)
+    
+for x in [1,2,3,4]:
+    print(x)
