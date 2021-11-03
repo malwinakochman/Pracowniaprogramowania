fb54ab8d6b999e01364b034d41400568fcce6aa9
malwinakochman <92380332+malwinakochman@users.noreply.github.com>
2021-11-03 01:33:06 +0100

    Update cwz4lekcjii.py

--- after4/cwz4lekcjii.py
+++ after4/cwz4lekcjii.py
@@ -1,10 +1,1 @@
-#19
 
-def is_within_the_given_range(a, x, y):
-    is_the_given = False
-    if a>x and a<y:
-        is_the_given = True
-    
-    return is_the_given
-
-print(is_within_the_given_range(1,2,5))
